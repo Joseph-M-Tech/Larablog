0d88c845c679321e8811f1f7c09b0158c7d7f5ca
Joseph_mwangi <joseph.portuit@gmail.com>
2023-04-15 15:28:09 +0300

    nav-bar adjustment

--- resources/views/Components/navbar.blade.php
+++ resources/views/Components/navbar.blade.php
@@ -1,10 +1,1 @@
 <?php
-<nav>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Resources</a></li>
-        <li><a href="#">Shop</a></li>
-    </ul>
-</nav>
